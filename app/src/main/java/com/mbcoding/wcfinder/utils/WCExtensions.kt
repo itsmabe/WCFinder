2d3ecfef0559ab34895e575ee03b5079c0b6ff0c
itsmabe <mahmoud_benmoussa@yahoo.fr>
2024-11-04 23:00:30 +0100

    fix location extension

--- app/src/main/java/com/mbcoding/wcfinder/utils/WCExtensions.kt
+++ app/src/main/java/com/mbcoding/wcfinder/utils/WCExtensions.kt
@@ -1,13 +1,16 @@
 package com.mbcoding.wcfinder.utils
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.LatLng
+import kotlinx.coroutines.tasks.await
 
-/*@SuppressLint("MissingPermission")
+@SuppressLint("MissingPermission")
 suspend fun Context.currentLocation(): LatLng? {
     if (!this.hasLocationPermission())
         throw SecurityException("Location permission not granted")
@@ -18,9 +21,7 @@ suspend fun Context.currentLocation(): LatLng? {
     } catch (e: Exception) {
         null
     }
-}*/
-suspend fun Context.currentLocation() = LatLng(48.876253, 2.301943)
-
+}
 
 fun Context.hasLocationPermission() = ContextCompat.checkSelfPermission(
     this,
